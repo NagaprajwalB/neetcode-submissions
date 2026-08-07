25dabb6d70e844dbf16dfe45c417199ac50b5817
Nagaprajwal B <136093225+NagaprajwalB@users.noreply.github.com>
2026-08-07 20:39:06 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 12
+float_type = 13.3
+bool_type = False
+string_type = "something"
+list_type = ["nem","singh"]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
