ed6e9d29cdc9629c96f5cf7dac0936509eb9bc00
Nagaprajwal B <136093225+NagaprajwalB@users.noreply.github.com>
2026-08-07 20:42:28 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 3
+print(type(variable))
+variable = 3.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "dfkasdf"
+print(type(variable))
+variable = [1,3,4]
+print(type(variable))
